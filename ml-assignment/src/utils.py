368e497f27efc6b2440b0c02139e29f92130a1d6
Priya T S <mjpriya0906@gmail.com>
2025-11-20 21:47:51 +0530

    Update utils.py

--- ml-assignment/src/utils.py
+++ ml-assignment/src/utils.py
@@ -1,2 +1,17 @@
 # This file is optional.
 # You can add any utility functions you need for your implementation here.
+import re
+
+def clean_text(text):
+    """
+    Lowercase and remove punctuation.
+    """
+    text = text.lower()
+    text = re.sub(r'[^a-z\s]', '', text)
+    return text
+
+def tokenize(text):
+    """
+    Split cleaned text into word tokens.
+    """
+    return text.split()
